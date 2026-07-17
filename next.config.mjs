6ce9459a9d47bdc47614cd96468a2c93bf70a22d
itsolutionstechhub <itsolutionstechhub@gmail.com>
2026-07-17 21:10:09 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // We use standard <img> tags, so no remotePattern configuration is strictly required for <Image>.
+};
+
+export default nextConfig;
