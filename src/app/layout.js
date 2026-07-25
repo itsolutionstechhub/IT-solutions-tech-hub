afab19321718f755cbdb1528ed5da015b4023bed
itsolutionstechhub <itsolutionstechhub@gmail.com>
2026-07-25 23:36:51 +0530

    Add files via upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -45,6 +45,19 @@ export default function RootLayout({ children }) {
           crossorigin="anonymous"
         ></script>
         <meta name="google-adsense-account" content="ca-pub-4127818731395844" />
+
+        {/* Microsoft Clarity Tracking Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xs39qdcbmm");
+            `,
+          }}
+        />
       </head>
       <body>
         <Header />
